25bbf97fd2035b888aef1ec51cb2f69df48b38a0
Evan Giesel <evan.giesel@gmail.com>
2016-06-17 13:16:59 -0500

    compile to es5 as well

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 var babel = require('babel-core');
 var fs = require('fs');
 
-var results = babel.transformFileSync('src/components.js', {"plugins": ["transform-react-jsx"], minified:true});
+var results = babel.transformFileSync('src/components.js', {"plugins": ["transform-react-jsx"], "presets": ["es2015"], minified:true});
 fs.writeFileSync('dist/dist.min.js', results.code)
